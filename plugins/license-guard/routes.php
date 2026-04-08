b6d28f413e2dcb315969ceaafad0279e0ebd3f50
0wum0 <florian0engelhardt@gmail.com>
2026-04-08 04:46:52 +0200

    refactor: license-guard Plugin entfernt, enabled.json aktualisiert - enabled.json license-guard aus Plugin-Array entfernt, plugins/license-guard-Directory komplett gelöscht (Plugin.php mit checkLicense/onlineCheck/applyToContext/enforceRestrictions/hasFeature-Logic, LicenseSetupController mit index/save/getSetting/saveSetting-Methods, ServiceProvider mit register/registerRoutes, manifest.json, routes.php, license-setup.twig-Template)

--- plugins/license-guard/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Plugins\LicenseGuard\LicenseSetupController;
-
-$router->get('/license-setup',  [LicenseSetupController::class, 'index']);
-$router->post('/license-setup', [LicenseSetupController::class, 'save']);
