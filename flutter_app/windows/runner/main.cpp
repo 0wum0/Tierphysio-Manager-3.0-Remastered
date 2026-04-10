2fe935a65940146917e42f794b5508ac9ef5d781
0wum0 <florian0engelhardt@gmail.com>
2026-04-10 20:57:55 +0200

    feat: Flutter Windows App - Fenster beim Start zentriert statt fester Position
    - Desktop-Bildschirmgröße wird via GetDesktopWindow() und GetWindowRect() ermittelt
    - Origin-Position berechnet als (screenWidth - windowWidth) / 2 und (screenHeight - windowHeight) / 2
    - Ersetzt feste Position (10, 10) durch dynamische Zentrierung bei 1280x720 Fenstergröße

--- flutter_app/windows/runner/main.cpp
+++ flutter_app/windows/runner/main.cpp
@@ -25,8 +25,17 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
+  
+  // Center window on screen
+  HWND desktop = GetDesktopWindow();
+  RECT rect;
+  GetWindowRect(desktop, &rect);
+  int screenWidth = rect.right - rect.left;
+  int screenHeight = rect.bottom - rect.top;
+  
   Win32Window::Size size(1280, 720);
+  Win32Window::Point origin((screenWidth - size.width) / 2, (screenHeight - size.height) / 2);
+  
   if (!window.Create(L"therapano", origin, size)) {
     return EXIT_FAILURE;
   }
